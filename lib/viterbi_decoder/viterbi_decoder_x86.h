eb953a4a03ce5525c54e3a0ff2a042e817bf7875
Gino Ledesma <gledesma@apple.com>
2020-02-14 13:27:23 -0800

    ARM-friendly build
    - Backport the generic version of viterbi decoder from 3.8 to 3.7
    - Adjust the build script to detect SSE2 capability (not supported by ARM)

--- lib/viterbi_decoder/viterbi_decoder_x86.h
+++ lib/viterbi_decoder/viterbi_decoder_x86.h
@@ -14,41 +14,27 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef INCLUDED_IEEE802_11_VITERBI_DECODER_H
-#define INCLUDED_IEEE802_11_VITERBI_DECODER_H
+#ifndef INCLUDED_IEEE802_11_VITERBI_DECODER_X86_H
+#define INCLUDED_IEEE802_11_VITERBI_DECODER_X86_H
 
 #include <xmmintrin.h>
-#include "utils.h"
+#include "base.h"
 
 namespace gr {
 namespace ieee802_11 {
-
-// Maximum number of traceback bytes
-#define TRACEBACK_MAX 24
 
 /* This Viterbi decoder was taken from the gr-dvbt module of
  * GNU Radio. It is an SSE2 version of the Viterbi Decoder
  * created by Phil Karn. The SSE2 version was made by Bogdan
  * Diaconescu. For more info see: gr-dvbt/lib/d_viterbi.h
  */
-class viterbi_decoder
+class viterbi_decoder : public base
 {
 public:
 
-	viterbi_decoder();
-	virtual ~viterbi_decoder();
-
-	uint8_t* decode(ofdm_param *ofdm, frame_param *frame, uint8_t *in);
+	virtual uint8_t* decode(ofdm_param *ofdm, frame_param *frame, uint8_t *in);
 
 private:
-
-	// Position in circular buffer where the current decoded byte is stored
-	int d_store_pos;
-	// Metrics for each state
-	unsigned char d_mmresult[64] __attribute__((aligned(16)));
-	// Paths for each state
-	unsigned char d_ppresult[TRACEBACK_MAX][64] __attribute__((aligned(16)));
-
 
 	union branchtab27 {
 		unsigned char c[32];
@@ -60,22 +46,8 @@ private:
 	__m128i d_path0[4] __attribute__ ((aligned(16)));
 	__m128i d_path1[4] __attribute__ ((aligned(16)));
 
-	int d_ntraceback;
-	int d_k;
-	ofdm_param *d_ofdm;
-	frame_param *d_frame;
-	const unsigned char *d_depuncture_pattern;
+	virtual void reset();
 
-	uint8_t d_depunctured[MAX_ENCODED_BITS];
-	uint8_t d_decoded[MAX_ENCODED_BITS * 3 / 4];
-
-	static const unsigned char PARTAB[256];
-	static const unsigned char PUNCTURE_1_2[2];
-	static const unsigned char PUNCTURE_2_3[4];
-	static const unsigned char PUNCTURE_3_4[6];
-
-	void reset();
-	uint8_t* depuncture(uint8_t *in);
 	void viterbi_chunks_init_sse2();
 	void viterbi_butterfly2_sse2(unsigned char *symbols,
 			__m128i m0[], __m128i m1[], __m128i p0[], __m128i p1[]);
@@ -86,4 +58,4 @@ private:
 } // namespace ieee802_11
 } // namespace gr
 
-#endif /* INCLUDED_IEEE802_11_VITERBI_DECODER_H */
+#endif /* INCLUDED_IEEE802_11_VITERBI_DECODER_X86_H */
